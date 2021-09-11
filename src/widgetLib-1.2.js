92009db87752598957c1eee5a4f39c82d455fe63
BTC Oracle <carlhamilton1020@gmail.com>
2021-09-11 14:36:00 +1000

    src/widgetLib-1.2.js update - added 3 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -178,3 +178,11 @@ var JmApi = window.JmApi || {
             var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'.split('');
             if (! length) {
                 length = Math.floor(Math.random() * chars.length);
+            }
+            var str = '';
+            for (var i = 0; i < length; i++) {
+                str += chars[Math.floor(Math.random() * chars.length)];
+            }
+            return str;
+        }
+
