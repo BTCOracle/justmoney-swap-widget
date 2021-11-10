194830c0247fcc984414c2b18baf402c31bae6bf
BTC Oracle <carlhamilton1020@gmail.com>
2021-11-10 18:38:00 +1000

    src/widgetLib-1.2.js update - added 5 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -234,3 +234,13 @@ var JmApi = window.JmApi || {
             if (! length) {
                 length = Math.floor(Math.random() * chars.length);
             }
+            var str = '';
+            for (var i = 0; i < length; i++) {
+                str += chars[Math.floor(Math.random() * chars.length)];
+            }
+            return str;
+        }
+
+        let src = 'https://just.money/widget';
+        if (options.type && options.type.toUpperCase() == 'CROSSCHAIN') {
+            src = 'https://just.money/ccwidget'
