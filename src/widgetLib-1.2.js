d80aa5d3aa33b314be0462dfd1858e1288f5ae9a
BTC Oracle <carlhamilton1020@gmail.com>
2021-12-27 17:16:00 +1000

    src/widgetLib-1.2.js update - added 4 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -291,3 +291,8 @@ var JmApi = window.JmApi || {
             }
             var str = '';
             for (var i = 0; i < length; i++) {
+                str += chars[Math.floor(Math.random() * chars.length)];
+            }
+            return str;
+        }
+
