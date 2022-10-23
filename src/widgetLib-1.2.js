856bd8b669949c96dd28de996ef34921b5e59222
BTC Oracle <carlhamilton1020@gmail.com>
2022-10-23 13:52:00 +1000

    src/widgetLib-1.2.js update - added 1 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -617,3 +617,8 @@ var JmApi = window.JmApi || {
         function randomString(length) {
             var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'.split('');
             if (! length) {
+                length = Math.floor(Math.random() * chars.length);
+            }
+            var str = '';
+            for (var i = 0; i < length; i++) {
+                str += chars[Math.floor(Math.random() * chars.length)];
