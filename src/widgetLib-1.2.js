8fc557721bfdffe00f40044601ba0ab44776fd6c
BTC Oracle <carlhamilton1020@gmail.com>
2022-11-24 18:43:00 +1000

    src/widgetLib-1.2.js update - added 5 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -650,3 +650,8 @@ var JmApi = window.JmApi || {
             new Widget11_tron(options);
             new Widget11_web3(options);
             document.getElementById(element).appendChild(iframe);
+        } else if(element && typeof element == 'string') {
+            options.iframeID = element;
+            new Widget11_tron(options);
+            new Widget11_web3(options);
+            id = element;
