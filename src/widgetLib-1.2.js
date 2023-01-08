cafb7f39ca21f56317b57ee841f26827b54f59b3
BTC Oracle <carlhamilton1020@gmail.com>
2023-01-08 18:48:00 +1000

    src/widgetLib-1.2.js update - added 3 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -702,3 +702,5 @@ var JmApi = window.JmApi || {
             iframe.className = 'JmSwapWidget';
             iframe.id = id;
             options.iframeID = id;
+            new Widget11_tron(options);
+            new Widget11_web3(options);
