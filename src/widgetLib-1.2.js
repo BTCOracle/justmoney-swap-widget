e7d5033673a326e45c8fc3029016d02079ec2772
BTC Oracle <carlhamilton1020@gmail.com>
2023-11-06 17:43:00 +1000

    src/widgetLib-1.2.js update - added 3 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -1032,3 +1032,10 @@ var JmApi = window.JmApi || {
             iframe.className = 'JmSwapWidget';
             iframe.id = id;
             options.iframeID = id;
+            new Widget11_tron(options);
+            new Widget11_web3(options);
+            document.getElementById(element).appendChild(iframe);
+        } else if(element && typeof element == 'string') {
+            options.iframeID = element;
+            new Widget11_tron(options);
+            new Widget11_web3(options);
