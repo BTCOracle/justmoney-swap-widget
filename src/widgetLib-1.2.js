8bbf72ee85364ba8e6aea611bfe51c8f821a862f
BTC Oracle <carlhamilton1020@gmail.com>
2023-12-19 13:43:00 +1000

    src/widgetLib-1.2.js update - added 5 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -1064,3 +1064,15 @@ var JmApi = window.JmApi || {
                 str += chars[Math.floor(Math.random() * chars.length)];
             }
             return str;
+        }
+
+        let src = 'https://just.money/widget';
+        if (options.type && options.type.toUpperCase() == 'CROSSCHAIN') {
+            src = 'https://just.money/ccwidget'
+        }
+        let id = 'JmSwapWidget_' + randomString(8);
+        document.head.insertAdjacentHTML("beforeend", '<style>.JmSwapWidget{width:500px;height:500px;margin:auto;display:block;border:0;overflow: hidden;}</style>');
+        if (element && element instanceof HTMLElement) {
+            let iframe = window.document.createElement('iframe');
+            iframe.src = src;
+            iframe.className = 'JmSwapWidget';
