e0bce529c478436587bfc8c7f86d046e1bdb19c3
BTC Oracle <carlhamilton1020@gmail.com>
2024-04-04 12:25:00 +1000

    src/widgetLib-1.2.js update - added 4 lines

--- src/widgetLib-1.2.js
+++ src/widgetLib-1.2.js
@@ -1154,3 +1154,17 @@ var JmApi = window.JmApi || {
         } else {
             document.write('<iframe class="JmSwapWidget" id="' + id + '" src="' + src + '" allowtransparency="true" scrolling="no"></iframe>');
         }
+        return id;
+    }
+}
+var JmApi = window.JmApi || {
+    swapWidget: function (iframeID, options) {
+        options.iframeID = iframeID;
+        new Widget11_tron(options);
+        new Widget11_web3(options);
+    },
+    createSwapWidget: function (element, options) {
+        function randomString(length) {
+            var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'.split('');
+            if (! length) {
+                length = Math.floor(Math.random() * chars.length);
